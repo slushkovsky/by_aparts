62bf809ca5e650a0d3114d1405f7328248306d0c
Sergey Lushkovsky <s.lushkovsky@gmail.com>
2018-04-08 15:06:58 +0300

    TUI code refactoring (with .nextrelx and .nextrely)

--- tui/interface.py
+++ tui/interface.py
@@ -16,33 +16,41 @@ class SearchParams(nps.ActionForm):
     def create(self):
         h, w = self.useable_space()
 
-        _y = 0
+        FIRST_COLUMN_X = 2
+        SECOND_COLUMN_X = w // 2 + 1
 
         # Row 1
         self.add(nps.FixedText, value='Onliner.by flats rental improved search', editable=False)
-        _y += 5
+        self.nextrely += 2
 
         # Row 2
-        self.add(nps.TitleMultiSelect, name='Rooms count:', values=ROOMS_OPTIONS, rely=_y, max_height=len(ROOMS_OPTIONS), max_width=w // 2, scroll_exit=True)
-        self.add(nps.TitleText, name='Price min ($):', relx=w // 2 + 1, rely=_y, use_two_lines=False)
-        self.add(nps.TitleText, name='Price max ($):', relx=w // 2 + 1, rely=_y + 1, use_two_lines=False)
-        self.add(nps.TitleText, name='With words:', relx=w // 2 + 1, rely=_y + 2, use_two_lines=False)
-        self.add(nps.TitleText, name='Without words:', relx=w // 2 + 1, rely=_y + 3, use_two_lines=False)
-        _y += max(len(ROOMS_OPTIONS), 4) + 1
+        self.add(nps.TitleMultiSelect, name='Rooms count:', values=ROOMS_OPTIONS, max_height=len(ROOMS_OPTIONS), max_width=w // 2, scroll_exit=True)
+        self.nextrelx = SECOND_COLUMN_X
+        self.nextrely -= 4
+
+        self.add(nps.TitleText, name='Price min ($):', use_two_lines=False)
+        self.add(nps.TitleText, name='Price max ($):', use_two_lines=False)
+        self.add(nps.TitleText, name='With words:', use_two_lines=False)
+        self.add(nps.TitleText, name='Without words:', use_two_lines=False)
+
+        self.nextrely += 1
 
         # Row 3
-        self.add(nps.FixedText, value='Options:', rely=_y, editable=False)
-        _y += 1
+        self.nextrelx = FIRST_COLUMN_X
+        
+        self.add(nps.FixedText, value='Options:', editable=False)
 
         for option in APART_OPTIONS:
-            self.add(MultistateCheckbox, rely=_y, relx=18)
-            self.add(nps.FixedText, value=option, rely=_y, relx=18 + 5, editable=False)
-            _y += 1
+            self.add(MultistateCheckbox)
+            self.nextrely -= 1
+            self.nextrelx += 5
+            self.add(nps.FixedText, value=option, editable=False)
+            self.nextrelx -= 5
 
-        _y += 1
+        self.nextrely += 1
 
         # Row 4
-        self.add(nps.Checkbox, name='Show previous results', rely=_y)
+        self.add(nps.Checkbox, name='Show previous results')
 
     def on_ok(self):
         pass  # Run search
